d91e0ad1eec4cc2f70936577d5bc5491d1a42840
Glauco Filho <glaucowow@hotmail.com.br>
2023-12-03 08:02:37 -0300

    remove_examples_to_optimize

--- src/schemas/pessoas.py
+++ src/schemas/pessoas.py
@@ -6,21 +6,22 @@ from pydantic import BaseModel, Field
 
 
 class CreatePessoaSchema(BaseModel):
-    apelido: str = Field(..., max_length=32, example="GlaucoSixx")
-    nome: str = Field(..., max_length=100, example="Glauco")
-    nascimento: date = Field(..., example=date(1900, 1, 1))
-    stack: Optional[list[str]] = Field(
-        ..., max_length=32, example=["Python", "FastAPI"]
-    )
+    apelido: str  # = Field(..., max_length=32, example="GlaucoSixx")
+    nome: str  # = Field(..., max_length=100, example="Glauco")
+    nascimento: date  # = Field(..., example=date(1900, 1, 1))
+    stack: Optional[list[str]]  # = Field(
+    #     ..., max_length=32, example=["Python", "FastAPI"]
+    # )
 
 
 class PessoaSchema(CreatePessoaSchema):
     id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4)
+    stack: Optional[str]
 
 
 class ReturnPessoaSchema(BaseModel):
     id: uuid.UUID
-    apelido: str = Field(..., max_length=32, example="GlaucoSixx")
-    nome: str = Field(..., max_length=100, example="Glauco")
-    nascimento: date = Field(..., example=date(1900, 1, 1))
-    stack: Optional[str] = Field(..., example=["Python", "FastAPI"])
+    apelido: str  # = Field(..., max_length=32, example="GlaucoSixx")
+    nome: str  # = Field(..., max_length=100, example="Glauco")
+    nascimento: date  # = Field(..., example=date(1900, 1, 1))
+    stack: Optional[str]
